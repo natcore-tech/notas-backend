92e11091622c37fae51b94dca86474f7b67897de
natcore-tech <elihu.navarrete@ute.edu.ec>
2026-06-05 00:51:32 -0500

    fix csrf origins

--- config/settings.py
+++ config/settings.py
@@ -8,6 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY    = config('SECRET_KEY')
 DEBUG         = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', cast=Csv())
 
 INSTALLED_APPS = [
     'django.contrib.admin',
